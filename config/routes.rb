d690f46c2b8573b2edaf342ade9af18c679dbad8
jarzayus <jarzayus@gmail.com>
2014-02-17 03:04:27 -0500

    added new controller and files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RailsTraining::Application.routes.draw do
+  get "demo/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
